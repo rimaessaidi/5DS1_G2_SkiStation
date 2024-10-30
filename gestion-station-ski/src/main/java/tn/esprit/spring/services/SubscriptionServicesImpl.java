7f67fdc94513545300d0eb2fb73de47ce0554691
dhiabenammar <dhia.benammar@esprit.tn>
2024-10-31 00:13:46 +0100

    com1

--- gestion-station-ski/src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
+++ gestion-station-ski/src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
@@ -25,91 +25,56 @@ public class SubscriptionServicesImpl implements ISubscriptionServices{
 
     @Override
     public Subscription addSubscription(Subscription subscription) {
-        log.info("Adding new subscription: {}", subscription);
-
-        try {
-            switch (subscription.getTypeSub()) {
-                case ANNUAL:
-                    subscription.setEndDate(subscription.getStartDate().plusYears(1));
-                    break;
-                case SEMESTRIEL:
-                    subscription.setEndDate(subscription.getStartDate().plusMonths(6));
-                    break;
-                case MONTHLY:
-                    subscription.setEndDate(subscription.getStartDate().plusMonths(1));
-                    break;
-                default:
-                    log.warn("Unrecognized subscription type: {}", subscription.getTypeSub());
-            }
-
-            Subscription savedSubscription = subscriptionRepository.save(subscription);
-            log.info("Successfully added subscription with ID: {}", savedSubscription.getNumSub());
-            return savedSubscription;
-
-        } catch (Exception e) {
-            log.error("Error adding subscription", e);
-            return null;
+        switch (subscription.getTypeSub()) {
+            case ANNUAL:
+                subscription.setEndDate(subscription.getStartDate().plusYears(1));
+                break;
+            case SEMESTRIEL:
+                subscription.setEndDate(subscription.getStartDate().plusMonths(6));
+                break;
+            case MONTHLY:
+                subscription.setEndDate(subscription.getStartDate().plusMonths(1));
+                break;
         }
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
     public Subscription updateSubscription(Subscription subscription) {
-        log.info("Updating subscription: {}", subscription);
-        try {
-            Subscription updatedSubscription = subscriptionRepository.save(subscription);
-            log.info("Successfully updated subscription with ID: {}", updatedSubscription.getNumSub());
-            return updatedSubscription;
-
-        } catch (Exception e) {
-            log.error("Error updating subscription", e);
-            return null;
-        }
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
     public Subscription retrieveSubscriptionById(Long numSubscription) {
-        log.debug("Retrieving subscription by ID: {}", numSubscription);
-        return subscriptionRepository.findById(numSubscription).orElseGet(() -> {
-            log.warn("No subscription found with ID: {}", numSubscription);
-            return null;
-        });
+        return subscriptionRepository.findById(numSubscription).orElse(null);
     }
 
     @Override
     public Set<Subscription> getSubscriptionByType(TypeSubscription type) {
-        log.info("Retrieving subscriptions of type: {}", type);
         return subscriptionRepository.findByTypeSubOrderByStartDateAsc(type);
     }
 
     @Override
     public List<Subscription> retrieveSubscriptionsByDates(LocalDate startDate, LocalDate endDate) {
-        log.info("Retrieving subscriptions between dates: {} and {}", startDate, endDate);
         return subscriptionRepository.getSubscriptionsByStartDateBetween(startDate, endDate);
     }
 
     @Override
-    @Scheduled(cron = "*/30 * * * * *")
+    @Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 secondes */
     public void retrieveSubscriptions() {
-        log.info("Retrieving all subscriptions at scheduled interval");
-        subscriptionRepository.findDistinctOrderByEndDateAsc().forEach(sub -> {
-            Skier aSkier = skierRepository.findBySubscription(sub);
-            log.info("Subscription ID: {}, End Date: {}, Skier: {} {}",
-                    sub.getNumSub(), sub.getEndDate(),
-                    aSkier.getFirstName(), aSkier.getLastName());
-        });
+        for (Subscription sub: subscriptionRepository.findDistinctOrderByEndDateAsc()) {
+            Skier   aSkier = skierRepository.findBySubscription(sub);
+            log.info(sub.getNumSub().toString() + " | "+ sub.getEndDate().toString()
+                    + " | "+ aSkier.getFirstName() + " " + aSkier.getLastName());
+        }
     }
 
    // @Scheduled(cron = "* 0 9 1 * *") /* Cron expression to run a job every month at 9am */
     @Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 secondes */
     public void showMonthlyRecurringRevenue() {
-        log.info("Calculating monthly recurring revenue");
-        try {
-            Float revenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY)
-                    + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL) / 6
-                    + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL) / 12;
-            log.info("Monthly Revenue = {}", revenue);
-        } catch (Exception e) {
-            log.error("Error calculating monthly revenue", e);
-        }
+        Float revenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY)
+                + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL)/6
+                + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL)/12;
+        log.info("Monthly Revenue = " + revenue);
     }
 }
