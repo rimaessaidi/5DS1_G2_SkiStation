1d8fcbb51445486bd5562105f58b779b755d541d
dhiabenammar <dhia.benammar@esprit.tn>
2024-11-02 16:09:32 +0100

    @repo

--- src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
@@ -4,13 +4,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
-
+@Repository
 public interface ISubscriptionRepository extends CrudRepository<Subscription, Long> {
 
     @Query("select s from Subscription s where s.typeSub = :typeS order by s.startDate")
