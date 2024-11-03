994fca2fa49a4899234d5a9bd975cf3d48298702
SelimBamri <selim.bamri@ihec.u-carthage.tn>
2024-11-03 18:04:45 +0100

    Fixing Result entites imports

--- src/main/java/tn/esprit/spring/entities/Result.java
+++ src/main/java/tn/esprit/spring/entities/Result.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
